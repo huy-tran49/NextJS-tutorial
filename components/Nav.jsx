177f35de280b5c44e019e33b92f6746add961d26
Huy <h.qt193@gmail.com>
2023-08-20 15:30:11 -0700

    adding logo, buttons, users signin

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,19 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
     const isUserLoggedIn = true
+
+    const [ providers, setProviders ] = useState(null)
+    const [ toggleDropDown, setToggleDropDown] = useState(false)
+
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders()
+            setProviders(response)
+        }
+        
+        setProviders()
+    },[])
+
     return (
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href='/' className="flex gap-2 flex-center">
@@ -27,12 +40,68 @@ const Nav = () => {
                         className="black_btn">
                             Create Post
                         </Link>
+                        <button type="button" onClick={signOut} className="outline_btn">
+                            Sign Out
+                        </button>
+
+                        <Link href='/profile'>
+                            <Image
+                                src="/assets/images/profile.svg"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="profile"
+                            />
+                        </Link>
                     </div>
                 ):(
                     <>
+                    {providers &&
+                        Object.values(providers).map((provider) => (
+                            <button 
+                                type="button"
+                                key={provider.name}
+                                onClick={()=> signIn(provider.id)}
+                                className="black_btn"
+                            >
+                                Sign In
+                            </button>
+                        ))
+                    }
                     </>
                 )} 
             </div>
+            {/* Mobile Navigation */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                    <Image
+                        src="/assets/images/logo.svg"
+                        width={37}
+                        height={37}
+                        className="rounded-full"
+                        alt="profile"
+                        onClick={()=> setToggleDropDown((prev)=> !prev)}
+                    />
+                    </div>
+                ): (
+                    <>
+                    {providers &&
+                        Object.values(providers).map((provider) => (
+                            <button 
+                                type="button"
+                                key={provider.name}
+                                onClick={()=> signIn(provider.id)}
+                                className="black_btn"
+                            >
+                                Sign In
+                            </button>
+                        ))
+                    }
+                    </>
+                )}
+            </div>
+
         </nav>
     )
 }
